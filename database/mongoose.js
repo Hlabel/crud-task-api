54ceb71b212a6fafaa1f362c8155f38989cde108
Hlabel <peterhlabeli@gmail.com>
2022-02-06 09:54:19 +0200

    Commit retry

--- database/mongoose.js
+++ database/mongoose.js
@@ -3,9 +3,7 @@
 // takes care of models
 
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-
 mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
 .then(() => {
     console.log("DB connected successfully")
@@ -13,6 +11,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
 .catch((error) =>{
     console.log(error)
 });
-
-
 module.exports = mongoose;
